ec969d2a53fe1983e5eb37abee805353d231e873
MacGyver98 <maobetancur@gmail.com>
2025-05-04 23:07:51 -0500

    Adición CD y reparaciones DDD (#5)
    * boilerplate
    
    * feat(breed-module): Agregar base inicial para el módulo de breeds
    
    * Feature/breeds module (#1)
    
    * fix(breed-module):  Rutas y conexión de app.component con breeds.module
    feat(loading):  Componente loading para uso global
    
    * fix(server-render):  Remoción del serverrender del proyecto
    fix(loading): Presentación de estado "loading breeds"
    
    * feat(lazy-load-images): Carga lazy de las imágenes
    fix(server):  Limpieza de archivos obsoletos
    
    * feat(autocomplete):  Creación del autocomplete para ver los resultados
    
    * feat(selected-breeds:  Lógica para mostrar la raza elegida en el autocomplete
    
    * feat(image-grid):  Carga de imágenes por raza y subraza
    
    * docs(readme):  Documentación readme.md sobre los detalles técnicos del proyecto
    
    * fix(angular.json):  Fixing the `browser` dist folder
    
    * fix(server):  Remover configuración remanente del serverside render que no es útil para el despliegue
    
    * fix(deploy): Reparación del folder del building
    
    ---------
    
    Co-authored-by: Mauricio Betancur <mbetancur@elogytec.com>
    
    * Feature/breeds module (#2)
    
    * fix(breed-module):  Rutas y conexión de app.component con breeds.module
    feat(loading):  Componente loading para uso global
    
    * fix(server-render):  Remoción del serverrender del proyecto
    fix(loading): Presentación de estado "loading breeds"
    
    * feat(lazy-load-images): Carga lazy de las imágenes
    fix(server):  Limpieza de archivos obsoletos
    
    * feat(autocomplete):  Creación del autocomplete para ver los resultados
    
    * feat(selected-breeds:  Lógica para mostrar la raza elegida en el autocomplete
    
    * feat(image-grid):  Carga de imágenes por raza y subraza
    
    * docs(readme):  Documentación readme.md sobre los detalles técnicos del proyecto
    
    * fix(angular.json):  Fixing the `browser` dist folder
    
    * fix(server):  Remover configuración remanente del serverside render que no es útil para el despliegue
    
    * fix(deploy): Reparación del folder del building
    
    ---------
    
    Co-authored-by: Mauricio Betancur <mbetancur@elogytec.com>
    
    * Feature/ddd fixes (#4)
    
    * fix(use-cases):  Reparación por omisión de la implementación de los casos de uso
    
    * feat(CI/CD):  Creación del pipeline para desplegar desde el ingreso de código a `main`
    
    ---------
    
    Co-authored-by: Mauricio Betancur <mbetancur@elogytec.com>
    
    ---------
    
    Co-authored-by: Mauricio Betancur <mbetancur@elogytec.com>

--- src/app/breeds/application/usecases/get-breeds.usecase.ts
+++ src/app/breeds/application/usecases/get-breeds.usecase.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Breed } from '../../domain/models/breed.model';
-import { BreedRepository } from '../../domain/repositories/breed.repository';
+import { BreedApiRepository } from '../../infrastructure/repositories/breed-api.repository';
 
 @Injectable({ providedIn: 'root' })
 export class GetBreedsUseCase {
-  constructor(private breedRepository: BreedRepository) {}
+  constructor(private breedApiRepository: BreedApiRepository) {}
 
   execute(): Observable<Breed[]> {
-    return this.breedRepository.getAllBreeds();
+    return this.breedApiRepository.getAllBreeds();
   }
 }
